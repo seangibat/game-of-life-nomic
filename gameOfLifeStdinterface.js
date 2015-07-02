19ad36e2d80d58b345972e70107e48553c5e24f1
Sean Gibat <sgibat@gmail.com>
2015-07-02 13:40:26 -0700

    fix EOF issue

--- gameOfLifeStdinterface.js
+++ gameOfLifeStdinterface.js
@@ -2,6 +2,7 @@ var GameOfLife = require('./gameOfLife.js');
 var readline = require('readline');
 var iterations;
 var lineNumber = 0;
+var height = Infinity;
 var board = [];
 
 var rl = readline.createInterface({
@@ -11,37 +12,41 @@ var rl = readline.createInterface({
 });
 
 rl.on('line', function(line){
+  lineNumber++;
 
-  if (line === "EOF") {
-    rl.close();
+  if (lineNumber === 1) {
+    iterations = parseInt(line, 10);
+  }
+  else if (lineNumber === 2){
+    height = parseInt(line.split(' ')[1], 10);
+  }
+  else if (lineNumber >= 3) {
+    board[lineNumber-3] = line.split(" ").map(function(c){ return parseInt(c, 10) });
 
-    var game = new GameOfLife({
-      board: board,
-      iterations: iterations
-    });
+    if (lineNumber - 2 >= height) {
 
-    if (process.argv[2] === "--watch") {
-      var interval = parseInt(process.argv[3], 10);
-      game.printBoard();
+      rl.close();
 
-      var runner = setInterval(function(){
-        game.iterate();
+      var game = new GameOfLife({
+        board: board,
+        iterations: iterations
+      });
+
+      if (process.argv[2] === "--watch") {
+        var interval = parseInt(process.argv[3], 10);
         game.printBoard();
-        console.log("");
-        if (!iterations--) clearInterval(runner);
-      }, interval);
 
-    } else {
-      game.runGame();
-      game.printBoard();
+        var runner = setInterval(function(){
+          game.iterate();
+          game.printBoard();
+          console.log("");
+          if (!iterations--) clearInterval(runner);
+        }, interval);
+
+      } else {
+        game.runGame();
+        game.printBoard();
+      }
     }
   }
-  else if (lineNumber === 0) {
-    iterations = parseInt(line, 10);
-  } 
-  else if (lineNumber > 1) {
-    board[lineNumber-2] = line.split(" ").map(function(c){ return parseInt(c, 10) });
-  }
-
-  lineNumber++;
 });
